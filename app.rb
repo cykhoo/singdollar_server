ab6103e4477880c842081f669ea950d7ac106217
Chong-Yee Khoo <mail@cykhoo.com>
2014-02-09 23:06:59 +0800

    add rates route and rates_xml method to return rates xml

--- app.rb
+++ app.rb
@@ -10,3 +10,17 @@ set :cache, Dalli::Client.new
 get '/' do
   erb :index
 end
+
+get '/rates' do
+  rates_xml
+end
+
+def rates_xml
+  content_type :xml
+
+  settings.cache.fetch("exchange_rates") do
+    exchange_rates = SingDollar.create
+    settings.cache.set("exchange_rates", exchange_rates, 600)
+    exchange_rates
+  end.to_xml
+end

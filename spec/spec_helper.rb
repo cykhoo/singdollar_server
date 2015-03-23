f1a72c09d829125d7c6bfe068eecfc7a105975af
Chong-Yee Khoo <mail@cykhoo.com>
2015-03-23 08:03:32 +0800

    edit spec_helper to allow to work
    spec files just require this file

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,17 @@ ENV['RACK_ENV'] = 'test'
 require_relative '../singdollar_server'
 require 'rspec'
 require 'capybara/rspec'
+require 'rack/test'
+
+include Rack::Test::Methods
+
+class SingdollarServer
+  set :environment, :test
+end
+
+def app
+  SingdollarServer
+end
 
 Capybara.app = SingdollarServer
 
